a0ea4d830f01d0bb1a9f118ec993a9ca421c6933
wu <54333@qq.com>
2019-05-24 15:16:23 +0800

    'wzn'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MenuLink from './componts/MenuLink/MenuLink';
-import App from './componts/App/App';
-import Home from "./componts/Home/Home";
+import RooterIndex from "./route/index"
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
@@ -23,8 +22,7 @@ ReactDOM.render(
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-md-12">
-                        <Route path="/" component={App} exact></Route>
-                        <Route path="/Home" component={Home}></Route>
+                       <RooterIndex/>
                     </div>
                 </div>
             </div>
